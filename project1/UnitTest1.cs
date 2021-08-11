b5ccd47742482fbad19478bf3b7324531d10147c
Anatoly Cherenkov <anatoly.cherenkov@gmail.com>
2021-08-11 17:38:18 +0300

    add service messages

--- project1/UnitTest1.cs
+++ project1/UnitTest1.cs
@@ -1,32 +1,37 @@
 using System;
 using Xunit;
+using Xunit.Abstractions;
 
 namespace project1
 {
     public class UnitTest1
     {
+        private readonly ITestOutputHelper _testOutputHelper;
+
+        public UnitTest1(ITestOutputHelper testOutputHelper)
+        {
+            _testOutputHelper = testOutputHelper;
+        }
+
         [Fact]
         [Trait("Disabled","False")]
         public void PassingTest()
         {
             Assert.Equal(4, Add(2, 2));
+            _testOutputHelper.WriteLine("##teamcity[testMetadata type='image' value='green.png']");
         }
 
         int Add(int x, int y)
         {
             return x + y;
         }
-
+        
         [Fact]
-        [Trait("Disabled","True")]
-        public void FrameworkDependentTest()
+        [Trait("Disabled","False")]
+        public void FailingTest()
         {
-            Assert.Equal(".NETCoreApp,Version=v2.0", GetFramework());
-        }
-
-        string GetFramework()
-        {
-            return AppDomain.CurrentDomain.SetupInformation.TargetFrameworkName;
+            Assert.Equal(4, Add(2, 2));
+            _testOutputHelper.WriteLine("##teamcity[testMetadata type='image' value='red.png']");
         }
     }
 }
