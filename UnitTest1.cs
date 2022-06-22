1c051953c986ea19f2fabb84b13986853c82690c
Anatoly Cherenkov <anatoly.cherenkov@gmail.com>
2022-06-22 14:46:42 +0200

    recreate project

---
+++ UnitTest1.cs
@@ -0,0 +1,28 @@
+namespace xunit_sample;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+        Thread.Sleep(10 * 1000);
+    }
+    
+    [Fact]
+    public void Test2()
+    {
+        Thread.Sleep(10 * 1000);
+    }
+    
+    [Fact]
+    public void Test3()
+    {
+        Thread.Sleep(10 * 1000);
+    }
+    
+    [Fact]
+    public void Test4()
+    {
+        Thread.Sleep(10 * 1000);
+    }
+}
