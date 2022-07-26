f2a5a41dd2d0440a8ef378d932ae2f1b876d8cc2
Anatoly Cherenkov <anatoly.cherenkov@gmail.com>
2022-07-26 15:17:06 +0200

    add console output to tests

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -1,28 +1,41 @@
+using Xunit.Abstractions;
+
 namespace xunit_sample;
 
 public class UnitTest1
 {
+    private readonly ITestOutputHelper _testOutputHelper;
+
+    public UnitTest1(ITestOutputHelper testOutputHelper)
+    {
+        _testOutputHelper = testOutputHelper;
+    }
+
     [Fact]
     public void Test1()
     {
-        Thread.Sleep(10 * 1000);
+        Thread.Sleep(1 * 1000);
+        _testOutputHelper.WriteLine("Test1 complete.");
     }
     
     [Fact]
     public void Test2()
     {
-        Thread.Sleep(10 * 1000);
+        Thread.Sleep(1 * 1000);
+        _testOutputHelper.WriteLine("Test2 complete.");
     }
     
     [Fact]
     public void Test3()
     {
-        Thread.Sleep(10 * 1000);
+        Thread.Sleep(1 * 1000);
+        _testOutputHelper.WriteLine("Test3 complete.");
     }
     
     [Fact]
     public void Test4()
     {
-        Thread.Sleep(10 * 1000);
+        Thread.Sleep(1 * 1000);
+        _testOutputHelper.WriteLine("Test4 complete.");
     }
 }
