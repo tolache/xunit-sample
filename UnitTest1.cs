3f7c628757e30a30ffe79ce8733732a0cbddf76b
Anatoly Cherenkov <anatoly.cherenkov@gmail.com>
2022-07-26 18:42:19 +0200

    Revert "increase tests duration"
    This reverts commit 3436a5f7a77a9068b1dcc8a353c5f674527fa7bc.

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -14,28 +14,28 @@ public class UnitTest1
     [Fact]
     public void Test1()
     {
-        Thread.Sleep(10 * 1000);
+        Thread.Sleep(1 * 1000);
         _testOutputHelper.WriteLine("Test1 complete.");
     }
     
     [Fact]
     public void Test2()
     {
-        Thread.Sleep(10 * 1000);
+        Thread.Sleep(1 * 1000);
         _testOutputHelper.WriteLine("Test2 complete.");
     }
     
     [Fact]
     public void Test3()
     {
-        Thread.Sleep(10 * 1000);
+        Thread.Sleep(1 * 1000);
         _testOutputHelper.WriteLine("Test3 complete.");
     }
     
     [Fact]
     public void Test4()
     {
-        Thread.Sleep(10 * 1000);
+        Thread.Sleep(1 * 1000);
         _testOutputHelper.WriteLine("Test4 complete.");
     }
 }
